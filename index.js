e1107b9f18a689404f758870f2affd86dc651ef2
altomas <altomas@yandex.ru>
2017-04-05 17:52:52 +0200

    fixed port initialization

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var server = require('./app/server');
 var log    = require('./app/log')(module);
 var config          = require('./app/config');
 
-var port = config.get('port');
+var port = process.env.PORT || config.get('port');
 
 server.listen(port, function(){
     log.info('Express server listening on port: '

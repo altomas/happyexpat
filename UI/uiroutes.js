deae156807c614896a4fd8bcb29eec4d229bbd3d
altomas <altomas@yandex.ru>
2017-04-06 13:58:32 +0200

    Update frontend with <html> tags

--- UI/uiroutes.js
+++ UI/uiroutes.js
@@ -4,17 +4,17 @@ var log    = require('../app/log')(module);
 
 
 var uiRenderings = function (server){
+        //route to home page
         server.get('/', function (req, res) {
             console.log("home")
         jst.renderFile('./UI/home.html', {name: 'jst'}, 
             function(err, ctx) {
-                if(err){
-                 console.log(err)
-                 return;
-                }
+                    if(err){
+                        console.log(err)
+                        return;
+                    }
 
-                res.send(ctx);
-                
+                    res.send(ctx);
                 });
     })
 }

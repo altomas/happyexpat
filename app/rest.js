0d094b1543f6805b78ddf19ab1497a0e450de6d5
ZZamira <t@t.com>
2017-04-10 09:27:54 +0200

    rest response was updated to return single result object

--- app/rest.js
+++ app/rest.js
@@ -12,13 +12,7 @@ var restAPI = function (server){
             console.log(tracking)
 console.log(tracking.getStatus)
             tracking.getStatus(req.params.caseid,function (err, track) {
-                if (!err) {
-                    return res.send(track);
-                } else {
-                    res.statusCode = 500;
-                    log.error('Internal error(%d): %s',res.statusCode,err.message);
-                    return res.send({ error: err });
-                }
+                    return res.send( { error: err, tracking: track } );
             });
     });
 

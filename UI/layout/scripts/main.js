e29db5723bc93b0bdd9125f5cd8d6f001fdc186a
ZZamira <t@t.com>
2017-04-12 13:14:18 +0200

    UI was updated with preloader

--- UI/layout/scripts/main.js
+++ UI/layout/scripts/main.js
@@ -1,28 +1,78 @@
 $( document ).ready(function() {
-    var hostName =location.host;console.log(hostName);
 
     //Events ------------------------------------------
     $("#checkbutton").on("click", function(){
-        var caseId = getCaseId("caseOrderId");
-        $.ajax({           
-            url: "/api/" + caseId
-        }).then(function(data) {
-            // if (data.status == "5") {
+        var caseIdfield = $("#caseOrderId"),
+            submitBatton = $(this),
+            caseIdvalue = getCaseId(caseIdfield),
+            preloader = $(".preloader"),
+            circularG = $("#circularG"),
+            wrongAlert = $("#wrongIdAlert"),
+            visaStatus = $("#visaStatus");
 
-            // } else if (condition) {
-                
-            // } else {
-                
-            // } {}
-        console.log(data);
+        preloader.removeClass("displayNone");
+        circularG.css("display", "block"); 
+
+        $.ajax({           
+            url: "/api/getstatus/" + caseIdvalue
+        }).done(function(data) {
+            if(data.error) {
+                circularG.css("display", "none");
+                preloader.addClass("displayNone");
+            }
+            var visaSatatusText = null;
+            switch(data.tracking.status){
+                case 5: visaSatatusText = "In processing. Try next day";
+                        visaStatus.attr("data-color", "inProcessing");
+                        break;
+                case 10: visaSatatusText = "Processed! Wait for the letter.";
+                            visaStatus.attr("data-color", "processed");
+                            break;
+            }
+
+            if(visaSatatusText != null) {
+                caseIdfield.addClass("visibilityOff");
+                submitBatton.addClass("visibilityOff");
+                visaStatus.html(visaSatatusText).removeClass("displayNone");                   
+            }           
+        })
+        .fail(function() {
+           caseIdfield.addClass("error");
+           wrongAlert.removeClass("visibilityOff");
+        })
+        .always(function() {
+            circularG.css("display", "none");
+            preloader.addClass("displayNone");
         });
+    
+    ;
+    return false;
     });
     
     $( "#caseOrderId" ).blur(function() {
-   // console.log( "Handler for .blur() called." );
+   
     });
+
+    // $(document).on('click', function(e) {
+    //     if (!$(e.target).closest(".parent_block").length) {
+    //         $('.toggled_block').hide();
+    //     }
+    //     e.stopPropagation();
+    // });
+
+    $( "#caseOrderId" ).focus(function() {
+        var el = $(this);
+        if(el.hasClass("error")) {
+            el.removeClass("error");
+            $("#wrongIdAlert").addClass("visibilityOff");
+        }
+    });
+
+    $("#getStatusBnt").on("click", function() {
+
+    })
 });
 
-function getCaseId(fieldId) {
-    return $("#" + fieldId).val();
+function getCaseId(fieldId) {    
+    return fieldId.val();
 }
